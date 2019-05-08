06c9fad6d0bfd1ddc7e846afa9a679ae76e90f39
harsh-wal <harsh.d@westagilelabs.com>
2019-05-08 18:46:41 +0530

    added updated code

--- site/templates/resource.php
+++ site/templates/resource.php
@@ -1,7 +1,9 @@
 <?php snippet('tutorials_header'); ?>
+<?= css('style.php/tutorial.scss') ?>
 
- <div class="site">          
-    <main role="main">
+
+ <div class="site tutorial-container">          
+    <main role="main " class="container ">
 
         <article class="blog format_text">
         	
@@ -15,22 +17,26 @@
 				<?php echo kirbytext($page->text()) ?>
             	
 				<?php if($file = $page->file($page->graphicFile())) { ?>
-					<p><strong>Graphic File:
-					<a href="<?php echo $file->url() ?>">
-				 	   <?php echo html($file->filename()) ?>
-					</a></strong></p>
+					<div class="graphic-file-wrap">
+						<p class="file-wrapper"><strong>Graphic File:
+						<a href="<?php echo $file->url() ?>">
+						<?php echo html($file->filename()) ?>
+							<img src="../../assets/images/download.png" alt="no-image"/>
+						</a></strong></p>
+						<?php echo kirbytext($page->credits()) ?>
+					</div>
 				<?php } ?>
 				
-				<?php echo kirbytext($page->credits()) ?>
+				
 				
             <footer class="post-sub cf">
             	<div class="post-sub-button">
-    				<a href="<?php echo url() ?>/tutorials/">← Back</a>
+    				<a href="<?php echo url() ?>/tutorials/">< Back</a>
     			</div>
                  
                 <p>Share this on:
-					 <a href="https://twitter.com/intent/tweet?source=webclient&text=<?php echo rawurlencode($page->title()); ?>%20<?php echo rawurlencode ($page->url()); ?>%20<?php echo rawurlencode($site->title()) ?>" target="blank" title="Tweet this"><img src="http://www.indeeo.com/idraw/tutorials/assets/images/twitter.png" alt="Share on Twitter" width="39" height="39" class="share-button" /></a>
-					 <a href="http://www.facebook.com/sharer.php?u=<?php echo rawurlencode ($page->url());?>&t=<?php echo rawurlencode($page->title()); ?>" target="_blank" title="Share on Facebook"><img src="http://www.indeeo.com/idraw/tutorials/assets/images/facebook.png" alt="Share on Facebook" width="39" height="39" /></a>
+					 <a href="https://twitter.com/intent/tweet?source=webclient&text=<?php echo rawurlencode($page->title()); ?>%20<?php echo rawurlencode ($page->url()); ?>%20<?php echo rawurlencode($site->title()) ?>" target="blank" title="Tweet this" class="social-links"><img src="../../assets/images/twitter.png" alt="Share on Twitter" width="39" height="39" class="share-button" /></a>
+					 <a href="http://www.facebook.com/sharer.php?u=<?php echo rawurlencode ($page->url());?>&t=<?php echo rawurlencode($page->title()); ?>" target="_blank" title="Share on Facebook" class="social-links"><img src="../../assets/images/facebook.png" alt="Share on Facebook" width="39" height="39" class="share-button" /></a>
                 </p>    			
             </footer>
             
